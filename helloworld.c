62877e72cba2b08bcd1cabb7045ab5de507accb2
jhoncampos2313-ops <jhoncampos2313gmail.com>
2026-03-17 08:46:46 -0600

    prueba pthreads

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <pthread.h>
+
+void* foo(void* arg) {
+    printf("Created a new thread");
+    return NULL;
+}
 
 int main (){
+    pthread_t thread1;
+
+    pthread_create(&thread1, NULL, foo, NULL);
     printf("Hello World\n");
     return 0; 
 };
