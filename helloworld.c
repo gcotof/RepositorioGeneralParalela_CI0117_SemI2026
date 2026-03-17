b5aad19efe4a0a7d5d42a23a06763919ff161c00
Gabriel <gabrielcoto.cr@gmail.com>
2026-03-17 09:01:26 -0600

    Use two threads to print Hello World with IDs
    Rename thread function to helloworld and accept an int argument (cast and dereference) to print a thread-specific ID. Create two threads (id1, id2), join both, and print "Hello World (thread N)" from each thread, replacing the previous single-thread behavior.

--- helloworld.c
+++ helloworld.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void* foo(void* arg) {
-    printf("Created a new thread\n");
+void* helloworld(void* arg) {
+    int thread_id = *(int*)arg;  // Cast and dereference argument
+    printf("Hello World (thread %d)\n", thread_id);
     return NULL;
 }
 
-int main (){
-    pthread_t thread1;
+int main() {
+    pthread_t thread1, thread2;
 
-    pthread_create(&thread1, NULL, foo, NULL);
-    pthread_join(thread1, NULL);  // Espera al hilo
+    int id1 = 1;
+    int id2 = 2;
 
-    printf("Hello World\n");
-    return 0; 
+    pthread_create(&thread1, NULL, helloworld, &id1);
+    pthread_create(&thread2, NULL, helloworld, &id2);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    return 0;
 }
