bfa4e279450f607e30c9e38968ad459290a2a4bd
Gabriel <gabrielcoto.cr@gmail.com>
2026-03-17 08:54:54 -0600

    Join thread, fix printf newline, and clean up
    Ensure the spawned thread is joined so the program waits for it (pthread_join) and will reliably print its message. Also add a trailing newline to the thread printf for clean output, remove an extraneous semicolon after main's closing brace, and add a brief comment for clarity.

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,7 @@
 #include <pthread.h>
 
 void* foo(void* arg) {
-    printf("Created a new thread");
+    printf("Created a new thread\n");
     return NULL;
 }
 
@@ -10,6 +10,8 @@ int main (){
     pthread_t thread1;
 
     pthread_create(&thread1, NULL, foo, NULL);
+    pthread_join(thread1, NULL);  // Espera al hilo
+
     printf("Hello World\n");
     return 0; 
-};
+}
