16e26ec288978d8e886de567f164ba9667476cfb
Gabriel <gabrielcoto.cr@gmail.com>
2026-03-20 07:39:13 -0600

    Removed certain test comments.

--- Trabajo En Clase 1/helloworld.c
+++ Trabajo En Clase 1/helloworld.c
@@ -22,29 +22,9 @@ int main() {
         thread_ids[i] = i; 
         pthread_create(&threads[i], NULL, helloworld, &thread_ids[i]);
     }
-    //Esto funcionó, pero tiene el limitante que hay que definir previamente el número de hilos que se va a utilizar. 
-
 
     for (int i = 0; i < numHilos + 1; i++){
         pthread_join(threads[i], NULL);
     }
 
-
-
-
-    //Versión OG:
-    // pthread_t thread1, thread2;
-
-    // int id1 = 0;
-    // int id2 = 1;
-
-    // pthread_create(&thread1, NULL, helloworld, &id1);
-    // pthread_create(&thread2, NULL, helloworld, &id2);
-
-    // pthread_join(thread1, NULL);
-    // pthread_join(thread2, NULL);
-
-    // return 0;
 }
-
-//Cosas a resolver: Investigar cómo no tener que poner un id manualmente a cada thread (Que se adapte dependiendo de la cantidad de núcleos/threads).
