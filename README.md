b1670a6792c84a6723271c901c706afd8665533f
Gabriel <gabrielcoto.cr@gmail.com>
2026-03-27 08:12:32 -0600

    Add semaphore test and update dining philosophers main
    Add a new semaphore test program and scaffold dining philosophers code. Creates TrabajoEnClase3_DiningPhilosophers/semaphoretest1.c implementing a circular message-passing demo using semaphores (one semaphore per thread), aligned to the number of processors. Update .gitignore to ignore the semaphoretest1 binary and its dSYM. Modify README.md to add repository title and header. Update main.c to rename the printed cores message to "filósofos", add a think() implementation, and add empty stubs for get_forks, eat, and put_forks to scaffold the Dining Philosophers implementation.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# RepositorioGeneralParalela_CI0117_SemI2026
+
 ## Miembros del Grupo
+
 Gabriel Coto Fernández (C4E540)
 Jhon Campos ()
--- TrabajoEnClase3_DiningPhilosophers/main.c
+++ TrabajoEnClase3_DiningPhilosophers/main.c
@@ -12,11 +12,31 @@
 //To define which forks, we can use functions left and right: 
 // def left(i): return i;
 // def right(i):return (i + 1) % numHilos; 
+void* think(void* arg) {
+    int thread_id = *(int*)arg;
+    printf("El Filósofo %d está pensando...\n",thread_id);
+    sleep(2);
+    return NULL;
+}
+
+void* get_forks(void* arg){
+
+}
+
+void* eat(void* arg){
+
+}
+
+void* put_forks(void* arg){
+
+}
+
+
 
 int main() {
 
     long numHilos = sysconf(_SC_NPROCESSORS_ONLN);
-    printf("Cantidad de cores: %ld\n", numHilos);
+    printf("Cantidad de filósofos: %ld\n", numHilos);
 
     pthread_t threads[numHilos];
     int thread_ids[numHilos];
