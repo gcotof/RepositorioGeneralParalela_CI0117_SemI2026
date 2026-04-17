83f28837302c1c100f5a18be680f63708cff436b
Gabriel <gabrielcoto.cr@gmail.com>
2026-04-17 09:41:14 -0600

    Added passenger generation.
    Each passenger is initialized and assigned a random class. Then they are routed to their corresponding queues. (Class is randomized.)

--- Assignment 0: Enhanced Airport Check-In Simulation/main.c
+++ Assignment 0: Enhanced Airport Check-In Simulation/main.c
@@ -208,21 +208,67 @@ void* counter_thread(void *arg) {
     return NULL;
 }
 
+
+void create_passengers(int N) {
+    for (int i = 0; i < N; i++) {
+        Passenger *p = malloc(sizeof(Passenger));
+
+        p->id = i;
+        p->class = rand() % 3; // random class
+        p->wasServed = false;
+        p->priority_bumped = false;
+
+        struct timespec now;
+        clock_gettime(CLOCK_MONOTONIC, &now);
+        p->arrival_time = now.tv_sec * 1000 + now.tv_nsec / 1000000;
+
+        // Route to correct queue
+        if (p->class == ECONOMY) {
+            enqueue(&economyQueue, p);
+        } else if (p->class == BUSINESS) {
+            enqueue(&businessQueue, p);
+        } else {
+            enqueue(&internationalQueue, p);
+        }
+    }
+}
+
 int main() {
 	clock_gettime(CLOCK_MONOTONIC, &global_start); // captures the current time (the type of clock, where to store the result)
 	
 	int N = 10; // default value -> we need to add functionality to read input from terminal
 	passengers_remaining = N;
+
+
 	pthread_mutex_init(&remaining_mutex, NULL); // inicialization of the remaining_mutext for remaining_passengers
+
+	create_passengers(N);
 
     //Queue definitions: 
     queue_init(&economyQueue);
     queue_init(&businessQueue);
     queue_init(&internationalQueue);
 
+	int M = 3;
+	pthread_t threads[M];
+	Counter counters[M];
+
+	// Initialize counters
+	counters[0] = (Counter){.id=0, .type=COUNTER_ECONOMY, .state=OPEN, .passengers_served_since_break=0, .K_limit=3};
+	counters[1] = (Counter){.id=1, .type=COUNTER_BUSINESS, .state=OPEN, .passengers_served_since_break=0, .K_limit=3};
+	counters[2] = (Counter){.id=2, .type=COUNTER_INTERNATIONAL, .state=OPEN, .passengers_served_since_break=0, .K_limit=3};
+
+	// Create threads
+	for (int i = 0; i < M; i++) {
+		pthread_create(&threads[i], NULL, counter_thread, &counters[i]);
+	}
+
    
 
 	//at the end of the main...
+	for (int i = 0; i < M; i++) {
+    	pthread_join(threads[i], NULL);
+	}
 	clock_gettime(CLOCK_MONOTONIC, &global_end); // captures the current time when the simulation ends (type of clock, where to store the result)
 	double total_time = (global_end.tv_sec - global_start.tv_sec) + (global_end.tv_nsec - global_start.tv_nsec) / 1e9; // subtracts the difference between seconds and nanoseconds, then adds them together to get the total simulation time in seconds
 	return 0;
