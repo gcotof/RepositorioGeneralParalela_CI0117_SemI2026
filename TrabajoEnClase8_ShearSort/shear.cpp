06c564e06eef6aa39b23477503722eb48b1f4dde
jhoncampos2313-ops <jhoncampos2313@gmail.com>
2026-05-13 16:43:29 -0600

    pseudocódigo para sortRowsAlternativeDirection

--- TrabajoEnClase8_ShearSort/shear.cpp
+++ TrabajoEnClase8_ShearSort/shear.cpp
@@ -1,6 +1,27 @@
 /* PSEUDOCODIGO
-    
+    sortRowsAlternativeDirection (A, M) { // dónde A es la matriz y M las dimensiones de A
+        for (f = 1 to M) // para recorrer c/u de las filas de la matriz
+            ¿el indice de la fila (f) ES par? 
+                Si si, entonces ORDENAR la fila de izq a der (creciente)
+                Para ello, al algoritmo que se implemente se le deben compartir los siguientes
+                parámetros: 
+                    1) A = la matriz.
+                    2) f = la fila que se desea ordenar.
+                    3) M = el largo de la fila.
+            ¿el indice de la fila (f) NO es par?
+                Entonces, ORDENAR la fila de der a izq (decreciente)
+                Para ello, al algoritmo que se implemente se le deben compartir los mismos
+                parámetros que en el caso par.
 
+        // NOTA: En ambos casos, dentro del algoritmo de ordenamiento seleccionado,
+                 el indice de la columna va a ser el indice usado para moverse sobre 
+                 las filas... tal que al acceder a un elemento: A[f][indice de la columna]
+                 recordando que f se le pasa por parámetro
+    }
+
+    sortColumns(A, M) {
+        
+    }
 */
 
 /**
