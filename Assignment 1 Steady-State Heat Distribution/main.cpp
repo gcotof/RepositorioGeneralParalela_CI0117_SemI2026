e195f4de45d72c7e32e9c017c9548df46027f06d
jhoncampos2313-ops <jhoncampos2313@gmail.com>
2026-05-14 03:36:04 -0600

    update to main.cpp
    I added that the main must receive arguments, as well as an aux method to validate that said arguments meet the program requirements

--- Assignment 1 Steady-State Heat Distribution/main.cpp
+++ Assignment 1 Steady-State Heat Distribution/main.cpp
@@ -1,9 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
 
 
+bool parse_args(int argc, char* argv[], int &N, int &steps, int &threads) {
+    bool areValidArgs = false;
+    if (argc == 4) {
+        N = stoi(argv[1]);
+        steps = stoi(argv[2]);
+        threads = stoi(argv[3]);
+        if (N <= 0 || steps <= 0 || threads <= 0) 
+            cout << "N, steps and threads must be >= 1" << endl;
+        else 
+            areValidArgs = true;
+    } else 
+        cout << "The number of arguments is invalid! You should enter: 1) ./heat 2) N, 3) steps and 4) threads" << endl;
+    return areValidArgs;
+}
 
+/**
+ * 
+ * @param argc: cant de argumentos
+ * @param argv = array de strings con los argumentos
+ */
+int main(int argc, char* argv[]){
+    int N = 0, steps = 0, threads = 0;
+    if (parse_args(argc, argv, N, steps, threads)) { // si los arg dados son válidos
 
-int main(){
-
+    }
     return 0; 
 }
