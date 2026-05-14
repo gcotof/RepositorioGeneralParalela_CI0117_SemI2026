4811328e452be5ec1019c501d19724080ffb69c4
jhoncampos2313-ops <jhoncampos2313@gmail.com>
2026-05-14 03:05:16 -0600

    implementation of initialize interior
    this method initialize the interior points o f the cube with the average temperature of the borders

--- Assignment 1 Steady-State Heat Distribution/grid.cpp
+++ Assignment 1 Steady-State Heat Distribution/grid.cpp
@@ -24,3 +24,16 @@ void Grid::initialize_boundaries() {
         }
     }
 }
+
+/**
+ * Método encargado de inicializar la temp del interior del cubo.
+ */
+void Grid::initialize_interior() {
+    const double average = (100.0 + 100.0 + 100.0 + 0.0 + 100.0 + 100.0) / 6.0;
+    for (int i = 1; i <= N-2; i++) {
+        for (int j = 1; j <= N-2; j++) {
+            for (int k = 1; k <= N-2; k++) 
+                set(i, j, k, average); // c/u de los punteros del interior del cubo se inicializan en base al promedio de la temp de los bordes
+        }
+    }
+}
