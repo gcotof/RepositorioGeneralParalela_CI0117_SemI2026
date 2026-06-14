80d5175655d49f856e72a3bf998a7710ed3aee1b
jhoncampos2313-ops <jhoncampos2313@gmail.com>
2026-06-13 23:51:47 -0600

    Actualización main.cpp
    Prueba para el paso de argumentos.

--- TrabajoEnClase10_1DStencil/main.cpp
+++ TrabajoEnClase10_1DStencil/main.cpp
@@ -1,5 +1,6 @@
 #include <mpi.h>
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main (int argc, char* argv[]) {
@@ -9,9 +10,19 @@ int main (int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    cout << "Hello from rank " << rank << " of " << size << endl;
+    if (argc < 3) {
+        if (rank == 0)
+            cout << "Use: mpiexec -np <P> ./main <N> <M>" << endl;
+        MPI_Finalize();
+        return 1;
+    }
+    
+    int N = atoi(argv[1]); // dimensión del arreglo.
+    int M = atoi(argv[2]); // cant de iteraciones.
+
+    if (rank == 0)
+        cout << "N = " << N << " M = " << M << " P = " << size << endl;
 
     MPI_Finalize();
-
     return 0;
 }
