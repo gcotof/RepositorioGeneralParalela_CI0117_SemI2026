c07dc1f523f5858d67b73ab5a613743673171e51
jhoncampos2313-ops <jhoncampos2313@gmail.com>
2026-06-13 23:45:04 -0600

    Actualización en el main y properties.json.

---
+++ TrabajoEnClase10_1DStencil/main.cpp
@@ -0,0 +1,17 @@
+#include <mpi.h>
+#include <iostream>
+using namespace std;
+
+int main (int argc, char* argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    cout << "Hello from rank " << rank << " of " << size << endl;
+
+    MPI_Finalize();
+
+    return 0;
+}
