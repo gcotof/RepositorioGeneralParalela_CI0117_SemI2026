7d5c76eb160e9b9565d8fdfefbeec6fc98266318
jhoncampos2313-ops <jhoncampos2313@gmail.com>
2026-07-04 15:44:20 -0600

    Creación de cuda_blur.hpp
    Para comenzar con la implementación de CUDA en el proyecto.

---
+++ Assignment3_GPUImageProcessing/include/cuda_blur.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+// Versión GPU con CUDA 
+vector<unsigned char> gaussian_blur_cuda(vector<unsigned char>& gray, int width, int height, int radius = 1);
