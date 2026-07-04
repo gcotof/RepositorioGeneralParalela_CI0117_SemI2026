c3cd0787a74532c3af30f60e9df2271809802fb1
jhoncampos2313-ops <jhoncampos2313@gmail.com>
2026-07-04 16:08:34 -0600

    Actualización de cuda_blur.cu
    Implementación de blur_kernel con clamp de bordes. Considerando que un hilo CUDA calcula un pixel de salida, repliqué el clamp de la versión CPU

--- Assignment3_GPUImageProcessing/src/cuda_blur.cu
+++ Assignment3_GPUImageProcessing/src/cuda_blur.cu
@@ -17,3 +17,18 @@ static const float KERNEL_5x5[5][5] = {
     {1/256.f, 4/256.f, 6/256.f, 4/256.f, 1/256.f}
 };
 
+_global_ void blur_kernel(const unsigned char* in, unsigned char* out, const float* k, int width, int height, int radius, int ksize) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x >= width || y >= height) return;
+    float suma = 0.0f;
+    for (int ky = -radius; ky <= radius; ky++) {
+        for (int kx = -radius; kx <= radius; kx++) {
+            int yy = min(max(y + ky, 0), height - 1);
+            int xx = min(max(x + kx, 0), width - 1);
+            float peso = k[(ky + radius) * ksize + (kx + radius)];
+            suma += peso * (float)in[yy * width + xx];
+        }
+    }
+    out[y * width + x] = (unsigned char)suma;
+}
